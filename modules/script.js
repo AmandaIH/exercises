93bb2d778d7cf422d5b1d11447ca72b709f4dfa1
Amanda <amhu0002@stud.kea.dk>
2025-01-31 13:51:38 +0100

    modules og utils

---
+++ modules/script.js
@@ -0,0 +1,10 @@
+import { sayHi } from "./utils.js";
+sayHi("Amanda");
+
+import { getRandomNumber } from "./utils.js";
+getRandomNumber(2);
+console.log(getRandomNumber(1));
+
+import { $ } from "./utils.js";
+
+$("h1").textContent = "Dette er en H1'er";
---
+++ trialanderror/script.js
@@ -0,0 +1,24 @@
+// function greeting(firstName) {
+//   return `Hello ${firstName}`;
+// }
+// // const result = greeting("any name");
+// // const txt = `Greeting is ${greeting("a name")}`;
+
+// const personToGreet = "Amanda";
+// //function-kald er "det samme" som dets return-værdi
+
+// const greetingToAmanda = greeting(personToGreet);
+// //variablen lig med return-værdien
+
+// console.log(greetingToAmanda);
+// console.log(greeting("Amanda")); //to måder at gøre en ting på
+
+// function greeting(firstName) {
+//   return `Hello ${firstName}`;
+// }
+// function lengthOfStr(str) {
+//   return str.length;
+// }
+// const greetingToAmanda = greeting("Amanda");
+// console.log(lengthOfStr("Amanda")); //tæller bogstaverne
+// console.log(lengthOfStr(greeting("Amanda"))); //den tæller både hello mellemrum og navn
