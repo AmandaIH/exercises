3bfe4173513f120006dbe94e688e2b1143373d4e
Amanda <amhu0002@stud.kea.dk>
2025-02-09 21:38:01 +0100

    string øvelse

---
+++ name-parts/nameparts.js
@@ -0,0 +1,12 @@
+const firstName = "Peter";
+const middleName = "Heronimous";
+const lastName = "Lind";
+
+console.log(firstName.indexOf("P"));
+console.log(firstName.substring(0, 5));
+
+console.log(middleName.indexOf("H"));
+console.log(middleName.substring(0, 10));
+
+console.log(lastName.indexOf("L"));
+console.log(lastName.substring(0, 4));
