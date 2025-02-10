8d9d2adf8e741bf43cad52d64072d06c96d1ef38
Amanda <amhu0002@stud.kea.dk>
2025-02-10 08:50:18 +0100

    ændret lidt

--- name-parts/nameparts.js
+++ name-parts/nameparts.js
@@ -1,12 +1,20 @@
-const firstName = "Peter";
-const middleName = "Heronimous";
-const lastName = "Lind";
+const theName = "Peter Heronimous Lind";
+const firstName = theName.substring(0, theName.indexOf(" "));
+console.log("firstName", firstName);
 
-console.log(firstName.indexOf("P"));
-console.log(firstName.substring(0, 5));
+const middleName = theName.substring(
+  theName.indexOf(" ") + 1,
+  theName.lastIndexOf(" ")
+);
+console.log("middleName", middleName);
 
-console.log(middleName.indexOf("H"));
-console.log(middleName.substring(0, 10));
+const lastName = theName.substring(theName.lastIndexOf(" ") + 1);
+console.log("lastsName", lastName);
+// console.log(firstName.indexOf("P"));
+// console.log(firstName.substring(0, 5));
 
-console.log(lastName.indexOf("L"));
-console.log(lastName.substring(0, 4));
+// console.log(middleName.indexOf("H"));
+// console.log(middleName.substring(0, 10));
+
+// console.log(lastName.indexOf("L"));
+// console.log(lastName.substring(0, 4));
